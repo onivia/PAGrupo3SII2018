560425394be8608f1410bd14fe452227df1b7be3
onivia <ohnivia@gmail.com>
2018-08-08 20:44:23 -0500

    precedencia de operadores en java.

--- 8pm_08agosto2018_1/src/principal/Programa.java
+++ 8pm_08agosto2018_1/src/principal/Programa.java
@@ -2,6 +2,23 @@ package principal;
 
 public class Programa {
     public static void main(String[] args) {
-        System.out.println("hola hola grupo3");        
+        //Precedencia de Operadores
+        //se evalua de izq a dere
+        //parentesis ()
+        //incrementos y decrementos
+        //multiplicacion, division o modulo
+        //suma y resta
+        
+        int i = 10;
+        int j = 5;
+        int r = 5 + 2 * 3 + ++i - 4 + i + j--;
+        //i=11; j=4
+        //5 + 2 * 3 + 11 - 4 + 11 + 5
+        //5 + 6 + 11 - 4 + 11 + 5
+        //22 - 4 + 16
+        //18 + 16 = 34
+        System.out.println(r);
+        System.out.println(i);
+        System.out.println(j);
     }
 }
