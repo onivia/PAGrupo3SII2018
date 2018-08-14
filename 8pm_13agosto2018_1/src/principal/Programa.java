d51a42ac4a4927b48343809934f981087deaa22a
onivia <ohnivia@gmail.com>
2018-08-13 20:18:19 -0500

    conversion x casting entre variables primitivas.

---
+++ 8pm_13agosto2018_1/src/principal/Programa.java
@@ -0,0 +1,16 @@
+package principal;
+
+public class Programa {
+    public static void main(String[] args) {
+        int ivar1 = 100;
+        int ivar2 = 32432423;
+        short svar1 = (short)ivar1;
+        short svar2 = (short)ivar2;
+        
+        float fvar1 = 100.02f;
+        int ivar3 = (int)fvar1;
+        
+        System.out.println(svar1); //conversion x casting entre variables primitivas
+        System.out.println(ivar3);
+    }
+}
