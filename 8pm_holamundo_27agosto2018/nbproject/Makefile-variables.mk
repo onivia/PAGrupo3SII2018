8fc5a522acc92078fa3f4b657e97178b8ab18a3d
onivia <ohnivia@gmail.com>
2018-08-28 07:33:01 -0500

    Hola Mundo C++.

---
+++ 8pm_holamundo_27agosto2018/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=CygWin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/CygWin-Windows
+CND_ARTIFACT_NAME_Debug=8pm_holamundo_27agosto2018
+CND_ARTIFACT_PATH_Debug=dist/Debug/CygWin-Windows/8pm_holamundo_27agosto2018
+CND_PACKAGE_DIR_Debug=dist/Debug/CygWin-Windows/package
+CND_PACKAGE_NAME_Debug=8pmholamundo27agosto2018.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/CygWin-Windows/package/8pmholamundo27agosto2018.tar
+# Release configuration
+CND_PLATFORM_Release=CygWin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/CygWin-Windows
+CND_ARTIFACT_NAME_Release=8pm_holamundo_27agosto2018
+CND_ARTIFACT_PATH_Release=dist/Release/CygWin-Windows/8pm_holamundo_27agosto2018
+CND_PACKAGE_DIR_Release=dist/Release/CygWin-Windows/package
+CND_PACKAGE_NAME_Release=8pmholamundo27agosto2018.tar
+CND_PACKAGE_PATH_Release=dist/Release/CygWin-Windows/package/8pmholamundo27agosto2018.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
