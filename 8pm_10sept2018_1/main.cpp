6a3eb98b4591f9fa803a3b674c186752603eeae1
onivia <ohnivia@gmail.com>
2018-09-10 21:00:47 -0500

    ejemplo memoria.

---
+++ 8pm_10sept2018_1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int i1 = 10;
+const int i2 = 30;
+int i3;
+
+int main() {
+    int arr1[] = {10,20};
+    int a = 45;
+    
+    cout<<&a<<endl;
+    cout<<arr1<<endl;
+
+    return 0;
+}
+
